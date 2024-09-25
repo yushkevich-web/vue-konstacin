38ee7adff35a727af76aeeab5a5037fab5155981
Novik-Artem <avacod222@gmail.com>
2024-09-25 22:13:34 +0300

    added video

--- src/components/layouts/LocalSwitcher.vue
+++ src/components/layouts/LocalSwitcher.vue
@@ -6,25 +6,19 @@
     </select>
   </div>
 </template>
+
 <style lang="scss" module>
 @import "@/assets/styles/common.scss";
 .swither {
   .select {
-    font-size: 1.5rem;
-    opacity: 50%;
+    font-size: 1rem;
     color: $black;
     cursor: pointer;
     text-transform: uppercase;
-    @include custom(610) {
-      font-size: 1.2rem;
-    }
     .option {
-      font-size: 1.5rem;
-      opacity: 50%;
+      font-size: 1rem;
       color: $black;
-      @include custom(610) {
-        font-size: 1.2rem;
-      }
+      cursor: pointer;
     }
   }
 }
